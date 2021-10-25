4ae3da1c215c3eadb8db7a38ccee16620177121f
Marco Botticelli <marco.botticelli@ibm.com>
2021-10-25 17:38:33 +0200

    2.14*: Data for countries, step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -20,7 +20,48 @@ const Countries = (props) => {
 
   const TooMany = () => { return <div>Too many matches, specify another filter</div> }
 
+  const Weather = (props) => {
+    const [weather, setWeather] = useState({
+      "current": {
+        "temperature": 0,
+        "weather_icons": [],
+        "wind_speed": 0,
+        "wind_dir": "",
+      }
+    })
+    const api_key = process.env.REACT_APP_API_KEY
+    // variable api_key has now the value set in startup
+
+    const url_weather = `http://api.weatherstack.com/current?access_key=${api_key}&query=${props.capital}`
+
+    useEffect(() => {
+      console.log('effect')
+      axios
+        .get(url_weather)
+        .then(response => {
+          console.log('promise fulfilled')
+          setWeather(response.data)
+        })
+    }, [url_weather])
+
+    return (
+      <div>
+        <h2>Weather in {props.capital}</h2>
+        <div>
+          <b>temperature: </b> {weather.current.temperature}
+          <br />
+          <img src={weather.current.weather_icons[0]} alt="weather" width="50" height="50" />
+          <br />
+          <b>wind: </b> {weather.current.wind_speed} mph direction {weather.current.wind_dir}
+        </div>
+      </div>
+    );
+
+
+  }
+
   const SingleCoutry = ({ country }) => {
+
     return (
       <div>
         <h1> {country.name.common}</h1>
@@ -28,9 +69,11 @@ const Countries = (props) => {
         <p>population {country.population}</p>
         <h2>languages</h2>
         <ul>
-          {Object.values(country.languages).map((val, k) => <li k={k}>{val}</li>)}
+          {Object.values(country.languages).map((val, k) => <li key={k}>{val}</li>)}
         </ul>
         <img src={country.flags.svg} alt="flag" width="150" height="150" />
+        <Weather capital={country.capital} />
+
       </div>
     )
 
@@ -62,9 +105,8 @@ const App = () => {
     setFilter(name)
   }
 
-
   useEffect(() => {
-    console.log('effect')
+    console.log('effect countries')
     axios
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
